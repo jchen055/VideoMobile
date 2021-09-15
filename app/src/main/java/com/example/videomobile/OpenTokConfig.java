288d05e37c3dd2dfb19b8ec7b594599d2e8a4855
Chen Jian Liang <jianliang.chen5@gmail.com>
2021-09-15 17:09:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/videomobile/OpenTokConfig.java
@@ -0,0 +1,7 @@
+package com.example.videomobile;
+
+public class OpenTokConfig {
+    public static final String API_KEY = "47332281";
+    public static final String SESSION_ID = "1_MX40NzMzMjI4MX5-MTYzMTY3NTc5OTY5MX5udFBKQUFxZzQxaHhjS0lFeE5aRW10dmZ-fg";
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NzMzMjI4MSZzaWc9ZDhjYzY2YTgxN2M4ZmJhMGYzZjI2MGVmM2I2MmY5MzZiMWJkMzk3ZTpzZXNzaW9uX2lkPTFfTVg0ME56TXpNakk0TVg1LU1UWXpNVFkzTlRjNU9UWTVNWDV1ZEZCS1FVRnhaelF4YUhoalMwbEZlRTVhUlcxMGRtWi1mZyZjcmVhdGVfdGltZT0xNjMxNjc1ODUwJm5vbmNlPTAuOTYzMTE4OTQ1NTEyMTM1NCZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjMxNzYyMjQ5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+}
